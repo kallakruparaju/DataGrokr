b9b876d393e3c77d5087df8e8ca3318debbbe4fc
Kalla kruparaju <64892543+kallakruparaju@users.noreply.github.com>
2023-05-15 18:57:27 +0530

    Update Main.java

--- Java/AbstractShape/Main.java
+++ Java/AbstractShape/Main.java
@@ -3,12 +3,16 @@ package AbstractShape;
 
 public class Main {
     public static void main(String[] args) {
-        Dimensions square = new Square(2,4);
+Dimensions square = new Square(2,4);
         int squareArea = square.CalArea();
         System.out.println("The area of Square is: "+squareArea);
+        int squareperimeter = square.perimeter();
+        System.out.println("The perimeter of Square is: "+squareperimeter);
         Dimensions cube = new Cube(3, 4);
         int cubeArea = cube.CalArea();
         System.out.println("The area of Cube is: "+cubeArea);
+        int cubeperimeter = cube.perimeter();
+        System.out.println("The perimeter of Cube is: "+cubeperimeter);
          
     }
 }
